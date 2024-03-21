527e86791d0def1e70c940d4318cd4dfb72f984c
syimleang <syimleang@myseneca.ca>
2024-03-21 18:53:49 -0400

    Update main page and NavBar

--- src/App.js
+++ src/App.js
@@ -1,29 +1,33 @@
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+
+import NavBar from './Components/NavBar';
+import Footer from './Components/Footer';
+import Home from './pages/Home';
+import About from './pages/About';
+import Classes from './pages/Classes';
+import Membership from './pages/Membership';
+import Booking from './pages/Booking';
+import Contact from './pages/Contact';
 import './styles.css';
 
-import VideoBg from './assets/gymBgVid.mp4';
-
 const App = () => {
-  return <section className='mainPage'>
-    {/* overlay */}
-    <div className='overlay'></div>
-    {/* video */}
-    <video src={VideoBg} autoPlay loop muted></video>
-    {/* content */}
-    <div className="page__content">
-      <h1>Coming Soon</h1>
-      <h2>
-         We will let you know when we are ready
-      </h2>
-      <br/>
-      
-      <h3>"Pure Passion Pure Muay Thai"</h3>
-    </div>
-
-    <div className='location'>
-      <p>2210 Queen St E, Toronto, Ontario</p>
-    </div>
-  </section>
+  return (
+    <>
+      <NavBar />
+      <div className="container">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/classes" element={<Classes />} />
+          <Route path="/membership" element={<Membership />} />
+          <Route path="/booking" element={<Booking />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>  
+      </div>
+      <Footer />
+    </>
+  )
 };
 
 export default App;
---
+++ src/Components/Footer.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import './Footer.css';
+
+const Footer = () => {
+  return (
+    <div className="footer">
+        <ul>
+            <li>Instagram</li>
+            <li>Facebook</li>
+        <li>X</li>
+        </ul>
+        <ul>
+            <h4>CopyRight Chanachon Muay Thai</h4>
+        </ul>
+    </div>
+    
+    
+  )
+}
+
+export default Footer
